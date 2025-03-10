94892e7d26393960c218c525768d40c159868abe
AkashArasu <takash.arasu@gmail.com>
2025-03-09 19:27:56 -0700

    replaced token code

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"871f65ac1bf129edb222c3293a636ff4b67534
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "452803947"
+    serviceWorkerVersion: "4238583992"
   }
 });
